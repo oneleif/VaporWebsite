00423b9ee8571ff30cadc02775ea4ef782b44b11
hallux <halluxdev@gmail.com>
2022-08-17 16:37:33 -0400

    Added validations.

--- Sources/App/Models/User.swift
+++ Sources/App/Models/User.swift
@@ -152,6 +152,18 @@ extension User {
             links: links
         )
     }
+    
+    struct Create: Content {
+        var email: String
+        var password: String
+        var confirmPassword: String
+    }
 }
 
+extension User.Create: Validatable {
+    static func validations(_ validations: inout Validations) {
+        validations.add("email", as: String.self, is: !.empty && .email)
+        validations.add("password", as: String.self, is: !.empty && .count(8...), required: true)
+    }
+}
 
