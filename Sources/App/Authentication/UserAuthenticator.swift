ce99a3d065dfb67c330cc3c682e9d3abd0dbe81f
hallux <halluxdev@gmail.com>
2022-08-17 22:17:24 -0400

    Added comment regarding naming convention

--- Sources/App/Authentication/UserAuthenticator.swift
+++ Sources/App/Authentication/UserAuthenticator.swift
@@ -9,6 +9,7 @@ import Vapor
 import Fluent
 
 extension User: ModelAuthenticatable {
+    // Unable to change the static let protocol key over to match naming convention due to how Vapor has setup the protocol.
     static let usernameKey = \User.$email
     static let passwordHashKey = \User.$passwordHash
     
