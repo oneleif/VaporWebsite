6e980b74fb5ce1553fe732b0d44e049356eb1605
Leif <zmeriksen@icloud.com>
2022-08-19 14:56:52 -0600

    Add new username and password

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -27,8 +27,8 @@ public func configure(_ app: Application) throws {
             .postgres(
                 hostname: Environment.get("DATABASE_HOST") ?? "localhost",
                 port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
-                username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-                password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+                username: Environment.get("DATABASE_USERNAME") ?? "oneleif_dev",
+                password: Environment.get("DATABASE_PASSWORD") ?? "oneleif_password",
                 database: Environment.get("DATABASE_NAME") ?? "vapor_database"
             ),
             as: .psql
